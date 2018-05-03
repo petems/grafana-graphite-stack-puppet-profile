12064b733f06253701c9575ca07e2f550f4b4fad
petems <p.morsou@gmail.com>
2018-05-03 11:55:03 +0100

    First iteration of file based provisioning

--- environments/vagrant/modules/profiles/manifests/grafana.pp
+++ environments/vagrant/modules/profiles/manifests/grafana.pp
@@ -5,18 +5,25 @@ class profiles::grafana {
     install_method => 'repo',
   }
 
-  file { '/etc/grafana/provisioning/datasources/graphite.yaml':
+  Package['grafana']
+  -> file { '/etc/grafana/provisioning/datasources/graphite.yaml':
     ensure  => file,
     content => template('profiles/datasource_grafana.yaml.erb'),
   }
   ~> Service['grafana-server']
 
-  grafana_dashboard { 'CollectD Stats from Graphite':
-    ensure           => present,
-    grafana_url      => 'http://127.0.0.1:3000',
-    grafana_user     => 'admin',
-    grafana_password => 'admin',
-    content          => template('profiles/grafana_dashboard.json.erb'),
+  Package['grafana']
+  -> file { '/etc/grafana/provisioning/dashboards/base_stats.yaml':
+    ensure  => file,
+    content => template('profiles/base_stats.yaml.erb'),
   }
+  ~> Service['grafana-server']
+
+  Package['grafana']
+  -> file { '/etc/grafana/provisioning/dashboards/base_stas.json':
+    ensure  => file,
+    content => template('profiles/grafana_dashboard.json.erb'),
+  }
+  ~> Service['grafana-server']
 
 }
