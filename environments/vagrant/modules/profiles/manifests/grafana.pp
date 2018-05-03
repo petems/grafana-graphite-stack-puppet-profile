d982dcb61f3f29f2867194a4262434e33f41bfae
petems <p.morsou@gmail.com>
2018-05-03 10:43:16 +0100

    Change to use file provisoner

--- environments/vagrant/modules/profiles/manifests/grafana.pp
+++ environments/vagrant/modules/profiles/manifests/grafana.pp
@@ -4,18 +4,13 @@ class profiles::grafana {
     version        => '5.1.0',
     install_method => 'repo',
   }
-  ->
-  grafana_datasource { 'Graphite':
-    ensure           => present,
-    type             => 'graphite',
-    url              => 'http://127.0.0.1:80',
-    access_mode      => 'proxy',
-    is_default       => true,
-    grafana_url      => 'http://127.0.0.1:3000',
-    grafana_user     => 'admin',
-    grafana_password => 'admin',
+
+  file { '/etc/grafana/provisioning/datasources/graphite.yaml':
+    ensure  => file,
+    content => template('profiles/datasource_grafana.yaml.erb'),
   }
-  ->
+  ~> Service['grafana-server']
+
   grafana_dashboard { 'CollectD Stats from Graphite':
     ensure           => present,
     grafana_url      => 'http://127.0.0.1:3000',
